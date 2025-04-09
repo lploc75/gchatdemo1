605ee95bfe819bc96442502141accb39871b48e2
lploc75 <lpl070524@gmail.com>
2025-04-09 20:47:32 +0700

    chỉnh sửa file user

--- lib/gchatdemo1_web/UserActivityTracker.ex
+++ lib/gchatdemo1_web/UserActivityTracker.ex
@@ -1,12 +1,17 @@
 defmodule Gchatdemo1Web.UserActivityTracker do
   alias Gchatdemo1.Repo
 
-  def update_last_active(user) do
-    # Cắt microseconds
-    datetime = DateTime.utc_now() |> DateTime.truncate(:second)
+  def update_last_active(user_id) do
+    case Repo.get(Gchatdemo1.Accounts.User, user_id) do
+      nil ->
+        {:error, :not_found}
 
-    user
-    |> Ecto.Changeset.change(last_active_at: datetime)
-    |> Repo.update()
+      user ->
+        datetime = DateTime.utc_now() |> DateTime.truncate(:second)
+
+        user
+        |> Ecto.Changeset.change(last_active_at: datetime)
+        |> Repo.update()
+    end
   end
 end
